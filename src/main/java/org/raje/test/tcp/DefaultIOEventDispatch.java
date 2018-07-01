24eb8f78db70a167efaaecdd9ca186b7dd1e3b59
liaorong2017 <liaorong_bh@126.com>
2018-07-01 20:27:24 +0800

    优化

--- src/main/java/org/raje/test/tcp/DefaultIOEventDispatch.java
+++ src/main/java/org/raje/test/tcp/DefaultIOEventDispatch.java
@@ -1,17 +1,27 @@
 package org.raje.test.tcp;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import org.apache.http.nio.reactor.IOEventDispatch;
 import org.apache.http.nio.reactor.IOSession;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultIOEventDispatch implements IOEventDispatch {
+	// TODO Auto-generated method stub
+	private String content = "GET /index.html HTTP/1.1\r\nHost: 192.168.24.128:8080\r\nConnection: Keep-Alive\r\nUser-Agent: Apache-HttpAsyncClient/4.1.3 (Java/1.8.0_91)\r\n\r\n";
 
 	@Override
 	public void connected(IOSession session) {
-		// TODO Auto-generated method stub
-		session.channel().write(src)
-		
+
+		try {
+			session.channel().write(ByteBuffer.wrap(content.getBytes()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -23,13 +33,13 @@ public class DefaultIOEventDispatch implements IOEventDispatch {
 	@Override
 	public void outputReady(IOSession session) {
 		// TODO Auto-generated method stub
-		//new RuntimeException("outputReady").printStackTrace();
+		// new RuntimeException("outputReady").printStackTrace();
 	}
 
 	@Override
 	public void timeout(IOSession session) {
 		// TODO Auto-generated method stub
-		//new RuntimeException("timeout").printStackTrace();
+		// new RuntimeException("timeout").printStackTrace();
 	}
 
 	@Override
