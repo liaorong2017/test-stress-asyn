d0cea28512119cae1b85d862a391172bfa601bac
liaorong2017 <liaorong_bh@126.com>
2018-07-01 18:58:56 +0800

    新方法实现

---
+++ src/main/java/org/raje/test/tcp/DefaultSessionRequestCallback.java
@@ -0,0 +1,34 @@
+package org.raje.test.tcp;
+
+import org.apache.http.nio.reactor.SessionRequest;
+import org.apache.http.nio.reactor.SessionRequestCallback;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultSessionRequestCallback implements SessionRequestCallback{
+
+	@Override
+	public void completed(SessionRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void failed(SessionRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void timeout(SessionRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelled(SessionRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
